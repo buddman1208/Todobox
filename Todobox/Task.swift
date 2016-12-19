d2a1ab4a0b9c8350847c44c20d4435958ba65135
Junseok Oh <kotohana5706@edcan.kr>
2016-12-19 19:57:00 +0900

    Initialize TodoBox Project

---
+++ Todobox/Task.swift
@@ -0,0 +1,20 @@
+//
+//  Task.swift
+//  Todobox
+//
+//  Created by Junseok Oh on 2016. 12. 19..
+//  Copyright © 2016년 Junseok Oh. All rights reserved.
+//
+
+
+struct Task {
+    var title: String
+    var memo: String?
+    var done: Bool = false
+    
+    init(title: String, memo: String? = nil){
+        self.title = title
+        self.memo = memo
+        self.done = false
+    }
+}
--- Todobox/ViewController.swift
+++ Todobox/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource{
 
+    @IBOutlet var tableView: UITableView!
+    var tasks: [Task] = [
+        Task(title: "원준갓", memo: "일해라"),
+        Task(title: "원준갓1", memo: "일해라4"),
+        Task(title: "원준갓2", memo: "일해라3"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.backgroundColor = UIColor.green
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return tasks.count
     }
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        cell.detailTextLabel?.text = task.memo
+        return cell
+    }
+    
 }
 
